80129003325c9916cabdc2f50628c1280025ee04
82106 <wanki21@naver.com>
2021-05-18 00:52:33 +0900

    문자보내기성공

--- app/src/main/java/com/example/brain3/ui/Message.java
+++ app/src/main/java/com/example/brain3/ui/Message.java
@@ -1,17 +1,26 @@
 package com.example.brain3.ui;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.example.brain3.R;
 
 public class Message extends Activity {
     Context mContext;
     EditText smsNumber, smsTextContext;
+    Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +31,42 @@ public class Message extends Activity {
 
         smsNumber = (EditText) findViewById(R.id.smsNumber);
         smsTextContext = (EditText) findViewById(R.id.smsText);
+        send = findViewById(R.id.sendBtn);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(Message.this, Manifest.permission.SEND_SMS)
+                        == PackageManager.PERMISSION_GRANTED){
+                    sendSMS();
+                }else{
+                    ActivityCompat.requestPermissions(Message.this,new String[]{Manifest.permission.SEND_SMS},100);
+                }
+            }
+        });
     }
 
-    public void sendSMS(View v){
+    public void sendSMS(){
         String smsNum = smsNumber.getText().toString();
         String smsText = smsTextContext.getText().toString();
 
         if (smsNum.length()>0 && smsText.length()>0){
-            sendSMS(smsNum, smsText);
+            SmsManager mSmsManager = SmsManager.getDefault();
+            mSmsManager.sendTextMessage(smsNum, null, smsText, null, null);
+            Toast.makeText(getApplicationContext(), "전송 완료", Toast.LENGTH_LONG).show();
         }else{
             Toast.makeText(this, "모두 입력해 주세요", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void sendSMS(String smsNumber, String smsText){
-        SmsManager mSmsManager = SmsManager.getDefault();
-        mSmsManager.sendTextMessage(smsNumber, null, smsText, null, null);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull  int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==100&&grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            sendSMS();
+        }else{
+            Toast.makeText(getApplicationContext(),"Permission Denied!",Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
