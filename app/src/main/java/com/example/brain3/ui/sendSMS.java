a57c21a286b386b217e51c390ea91fceb10c23c6
82106 <wanki21@naver.com>
2021-06-08 01:54:37 +0900

    전화번호부 대성공

--- app/src/main/java/com/example/brain3/ui/sendSMS.java
+++ app/src/main/java/com/example/brain3/ui/sendSMS.java
@@ -1,18 +1,25 @@
 package com.example.brain3.ui;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -22,6 +29,7 @@ public class sendSMS extends Activity {
     Context mContext;
     EditText smsNumber, smsTextContext;
     Button send;
+    ImageButton call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +53,35 @@ public class sendSMS extends Activity {
                 }
             }
         });
+        call = findViewById(R.id.imageButton2);
+        call.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode==RESULT_OK)
+        {
+            Cursor cursor = getContentResolver().query(data.getData(),
+                    new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                    ContactsContract.CommonDataKinds.Phone.NUMBER},null,null,null);
+            cursor.moveToFirst();
+            smsNumber.setText(cursor.getString(1));
+            cursor.close();
+        }
+        super.onActivityResult(requestCode,resultCode,data);
     }
 
+
+
     public void sendSMS(){
+
         String smsNum = smsNumber.getText().toString();
         String smsText = smsTextContext.getText().toString();
 
@@ -55,12 +89,12 @@ public class sendSMS extends Activity {
             SmsManager mSmsManager = SmsManager.getDefault();
             mSmsManager.sendTextMessage(smsNum, null, smsText, null, null);
             Toast.makeText(getApplicationContext(), "전송 완료", Toast.LENGTH_LONG).show();
+            moveTaskToBack(true);
+            finish();
+            android.os.Process.killProcess(android.os.Process.myPid());
         }else{
             Toast.makeText(this, "모두 입력해 주세요", Toast.LENGTH_SHORT).show();
         }
-        android.os.Process.killProcess(android.os.Process.myPid());
-        moveTaskToBack(true);
-        finish();
     }
 
     @Override
